fecba40eaf39f746ec1ea051aba21f97d30f8677
wooginawunan <wooginawunan@gmail.com>
2022-04-18 12:46:30 -0400

    breast downstream tasks

---
+++ configs/benchmarks/classification/nyubreast/base.py
@@ -0,0 +1,73 @@
+# dataset settings
+data_prefix = '/gpfs/data/geraslab/Nan/data/breast_mml_datalists/20220111/breasts_lists/ffdm_screening_only/'
+data_source = 'NYUBreastScreening'
+
+# dataset summary
+data = dict(
+    samples_per_gpu=64,  # total 32*8=256
+    workers_per_gpu=20,
+    drop_last=False,
+    num_subsamples=4000,
+    train=dict(
+        data_source=dict(
+            type=data_source,
+            data_prefix=data_prefix+'full',
+            test_mode=False,
+        )),
+    val=dict(
+        data_source=dict(
+            type=data_source,
+            data_prefix=data_prefix+'balanced',
+            test_mode=True,
+        )))
+
+# prefetch
+prefetch = False
+img_norm_cfg = dict()
+
+
+# model
+model = dict(
+    type='Classification',
+    backbone=dict(
+        type='ResNet',
+        depth=18,
+        in_channels=1,
+        num_stages=4,
+        strides=(1, 2, 2, 2),
+        dilations=(1, 1, 1, 1),
+        out_indices=[4],  # 0: conv-1, x: stage-x
+        norm_cfg=dict(type='BN'),
+        #norm_cfg=dict(type='SyncBN'),
+        #zero_init_residual=True,
+        frozen_stages=4
+        ),
+    head=dict(
+        type='ClsHead', with_avg_pool=True, in_channels=512,
+        num_classes=2))
+
+train_cfg = {}
+test_cfg = {}
+optimizer_config = dict()  # grad_clip, coalesce, bucket_size_mb
+# yapf:disable
+log_config = dict(
+    interval=1,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        # dict(type='TensorboardLoggerHook')
+    ])
+# yapf:enable
+
+# runtime settings
+dist_params = dict(backend='nccl')
+cudnn_benchmark = True
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+persistent_workers = True
+
+# disable opencv multithreading to avoid system being overloaded
+opencv_num_threads = 0
+# set multi-process start method as `fork` to speed up the training
+mp_start_method = 'fork'
--- configs/benchmarks/classification/nyubreast/ffdm.py
+++ configs/benchmarks/classification/nyubreast/ffdm.py
@@ -1,106 +1,52 @@
-# dataset settings
-data_source = 'NYUBreastScreening'
-dataset_type = 'BreastFFDMClassification'
+_base_ = [
+    'base.py',
+]
 
-train_ffdm_pipeline = [
+dataset_type = 'BreastFFDMClassification'
+train_pipeline = [
     dict(type='RandomHorizontalFlip', p=0.5),
     dict(type='RandomVerticalFlip', p=0.5),
     dict(type='RandomAffine', degrees=(-10, 10), translate=(0.1,0.1), scale=(0.9, 1.1)),
-    dict(type='Resize', size=(736, 480)),
-    dict(type='ToNumpy'), # TODO
-    dict(type='ToTensor'),
-    dict(type='Standardizer'),
-    # dict(type='CopyChannel')
-]
-
-val_ffdm_pipeline = [
-    dict(type='Resize', size=(736, 480)),
+    dict(type='Resize', size=(1472, 960)),
     dict(type='ToNumpy'), # TODO
     dict(type='ToTensor'),
     dict(type='Standardizer'),
 ]
 
-# prefetch
-prefetch = False
-img_norm_cfg = dict()
+val_pipeline = [
+    dict(type='Resize', size=(1472, 960)),
+    dict(type='ToNumpy'), # TODO
+    dict(type='ToTensor'),
+    dict(type='Standardizer'),
+]
 
 # dataset summary
 data = dict(
-    samples_per_gpu=64,  # total 32*8=256
-    workers_per_gpu=20,
-    drop_last=True,
-    train=dict(
-        type=dataset_type,
-        data_source=dict(
-            type=data_source,
-            data_prefix='/gpfs/data/geraslab/Nan/data/breast_mml_datalists/20220111/breasts_lists/ffdm_screening_only/full',
-            test_mode=False,
-        ),
-        pipeline=train_ffdm_pipeline),
-    val=dict(
-        type=dataset_type,
-        data_source=dict(
-            type=data_source,
-            data_prefix='/gpfs/data/geraslab/Nan/data/breast_mml_datalists/20220111/breasts_lists/ffdm_screening_only/full',
-            test_mode=True,
-        ),
-        pipeline=val_ffdm_pipeline)
-    )
+    train=dict(type=dataset_type, pipeline=train_pipeline),
+    val=dict(type=dataset_type, pipeline=val_pipeline))
 
-evaluation = dict(interval=1, topk=(1))
-model = dict(
-    type='Classification',
-    backbone=dict(
-        type='ResNet',
-        depth=18,
-        in_channels=1,
-        num_stages=4,
-        strides=(1, 2, 2, 2),
-        dilations=(1, 1, 1, 1),
-        out_indices=[4],  # 0: conv-1, x: stage-x
-        norm_cfg=dict(type='BN'),
-        frozen_stages=4),
-    head=dict(
-        type='ClsHead', with_avg_pool=True, in_channels=512,
-        num_classes=2))
-
+model = dict(type='Classification')
 
 # optimizer
-optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=1e-6)
+optimizer = dict(type='SGD', lr=1e-4, momentum=0.9, weight_decay=1e-6)
 
 # learning policy
 lr_config = dict(policy='CosineAnnealing', min_lr=0.)
 
-# runtime settings
-runner = dict(type='EpochBasedRunner', max_epochs=100)
+# # optimizer
+# optimizer = dict(type='AdamW', lr=1e-3, betas=(0.9, 0.999), weight_decay=0.05)
 
-
-train_cfg = {}
-test_cfg = {}
-optimizer_config = dict()  # grad_clip, coalesce, bucket_size_mb
-# yapf:disable
-log_config = dict(
-    interval=50,
-    hooks=[
-        dict(type='TextLoggerHook'),
-        # dict(type='TensorboardLoggerHook')
-    ])
-# yapf:enable
+# # learning policy
+# lr_config = dict(
+#     policy='CosineAnnealing',
+#     min_lr=0.,
+#     warmup='linear',
+#     warmup_iters=5,
+#     warmup_ratio=1e-4,  # cannot be 0
+#     warmup_by_epoch=True)
 
 # runtime settings
-dist_params = dict(backend='nccl')
-cudnn_benchmark = True
-log_level = 'INFO'
-load_from = None
-resume_from = None
-workflow = [('train', 1)]
-persistent_workers = True
-
-# disable opencv multithreading to avoid system being overloaded
-opencv_num_threads = 0
-# set multi-process start method as `fork` to speed up the training
-mp_start_method = 'fork'
-
+runner = dict(type='EpochBasedRunner', max_epochs=400)
 
 # swav setting
 # runtime settings
@@ -108,3 +54,6 @@ mp_start_method = 'fork'
 # if it is 3, when CheckpointHook (in mmcv) saves the 4th ckpt
 # it will remove the oldest one to keep the number of total ckpts as 3
 checkpoint_config = dict(interval=1, max_keep_ckpts=3)
+
+evaluation = dict(interval=10)
+seed=3
